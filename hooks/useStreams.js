c37435c3f4aa5784df8c4988916a3fdf9ebe19a7
abhik-bits <127435906+abhik-bits@users.noreply.github.com>
2023-06-30 13:56:21 +0530

    Created Provider for Streams

--- hooks/useStreams.js
+++ hooks/useStreams.js
@@ -1,37 +1,4 @@
-import { useReducer } from "react";
+import { StreamsContext } from "@/providers/StreamsProvider";
+import { useContext } from "react";
 
-const ACTIONS = {
-  showStream: "show_stream",
-  showSendStream: "show_send_stream",
-  showXStream: "show_x_stream",
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case ACTIONS.showStream: {
-      return { ...state, showStream: action.payload };
-    }
-    case ACTIONS.showXStream: {
-      return { ...state, SendXStream: action.payload };
-    }
-    case ACTIONS.showSendStream: {
-      return { ...state, showSendStream: action.payload };
-    }
-  }
-  throw Error("Unknown action: " + action.type);
-}
-export const useStreams = () => {
-  const { state, dispatch } = useReducer(reducer, {
-    showXStream: false,
-    showStream: false,
-    showSendStream: false,
-  });
-
-  return {
-    ...state,
-    setShowStream: (v) => dispatch({ type: ACTIONS.showStream, payload: v }),
-    setShowXStream: (v) => dispatch({ type: ACTIONS.showXStream, payload: v }),
-    setSendStream: (v) =>
-      dispatch({ type: ACTIONS.showSendStream, payload: v }),
-  };
-};
+export const useStreams = () => useContext(StreamsContext);
--- providers/Providers.js
+++ providers/Providers.js
@@ -3,19 +3,24 @@ import { NotificationsProvider } from "./NotificationsProvider";
 import { WagmiConfig } from "wagmi";
 import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import { wagmiClient, chains } from "@/lib/wagmi";
+import { StreamsProvider } from "./StreamsProvider";
 
 const Providers = ({ children }) => {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}
-          theme={lightTheme({
-            accentColor: "#10bb35",
-            accentColorForeground: "white",
-            borderRadius: "medium",
-            fontStack: "system",
-            overlayBlur: "small",
-          })}>
-        <NotificationsProvider>{children}</NotificationsProvider>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({
+          accentColor: "#10bb35",
+          accentColorForeground: "white",
+          borderRadius: "medium",
+          fontStack: "system",
+          overlayBlur: "small",
+        })}
+      >
+        <StreamsProvider>
+          <NotificationsProvider>{children}</NotificationsProvider>
+        </StreamsProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
